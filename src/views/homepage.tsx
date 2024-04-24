f95455797d993eee9c175fa76788b33b55af3d01
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-24 07:34:54 +0300

    feat: adds hero container to homepage

---
+++ src/views/homepage.tsx
@@ -0,0 +1,10 @@
+import HeroContainer from "@/containers/hero";
+import * as React from "react";
+
+export default function HomepageView() {
+  return (
+    <div>
+      <HeroContainer />
+    </div>
+  );
+}
