236f51e341e8e40d9dbbd181226f8d89ea92ece2
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-24 07:33:46 +0300

    refactor: refactors head,header,homepage to use layout

--- src/layouts/head.tsx
+++ src/layouts/head.tsx
@@ -9,7 +9,10 @@ export default function HeadLayout() {
       <meta name="keywords" content="Rick and Morty, characters, search, universe" />
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" href="https://res.cloudinary.com/droheqpxn/image/upload/v1713917214/Screenshot_2024-04-24_at_03.05.28_gwupbp.png" />
+      <link
+        rel="icon"
+        href="https://res.cloudinary.com/droheqpxn/image/upload/v1713917214/Screenshot_2024-04-24_at_03.05.28_gwupbp.png"
+      />
 
       {/* Open Graph tags */}
       <meta property="og:title" content="Rick and Morty Search | Find your favorite characters!" />
@@ -20,7 +23,10 @@ export default function HeadLayout() {
       {/* Twitter tags */}
       <meta name="twitter:title" content="Rick and Morty Search | Find your favorite characters!" />
       <meta name="twitter:description" content="Explore characters from the Rick and Morty universe." />
-      <meta name="twitter:image" content="https://res.cloudinary.com/droheqpxn/image/upload/v1713918653/Screenshot_2024-04-24_at_03.05.28-removebg-preview_pyieib.png" />
+      <meta
+        name="twitter:image"
+        content="https://res.cloudinary.com/droheqpxn/image/upload/v1713918653/Screenshot_2024-04-24_at_03.05.28-removebg-preview_pyieib.png"
+      />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
