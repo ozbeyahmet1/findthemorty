92b408b4fad7a28e6fb47a8257718c561014a90e
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-24 20:11:31 +0300

    refactor: adds route to logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,6 @@
 import { Cabin_Sketch } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const jersey = Cabin_Sketch({
   weight: "700",
@@ -8,17 +9,19 @@ const jersey = Cabin_Sketch({
 
 export default function Logo() {
   return (
-    <div className="flex items-center gap-2 ">
-      <Image
-        src="https://res.cloudinary.com/droheqpxn/image/upload/v1713976518/SeekPng.com_rick-and-morty-png_44144_txzuta.png"
-        width={50}
-        height={80}
-        alt="Logo"
-      />
-      <div className={`leading-[0] h-[40px] `}>
-        <p className={`${jersey.className} text-xl`}>FIND MORTY</p>
-        <p className="text-xs">Step into the portal</p>
+    <Link href="/">
+      <div className="flex items-center gap-2">
+        <Image
+          src="https://res.cloudinary.com/droheqpxn/image/upload/v1713976518/SeekPng.com_rick-and-morty-png_44144_txzuta.png"
+          width={50}
+          height={80}
+          alt="Logo"
+        />
+        <div className={`leading-[0] h-[40px] `}>
+          <p className={`${jersey.className} text-xl`}>FIND MORTY</p>
+          <p className="text-xs">Step into the portal</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
